edd6b7fa4a6a379884ea63d3b8b625df503a08a0
Gokhun Celik <gokhun@gmail.com>
2023-01-12 23:33:53 +0100

    Setup project

---
+++ src/test/java/dev/gokhun/guavaspringconfigprops/ApplicationTests.java
@@ -0,0 +1,11 @@
+package dev.gokhun.guavaspringconfigprops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+final class ApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
